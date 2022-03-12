f0610697431dc0f8544bf62de422e61b561f4ac5
alanpelayoo <aln.10zepeda@gmail.coom>
2022-03-11 18:35:34 -0600

    make it better 2

--- expensesmgmt/settings.py
+++ expensesmgmt/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-import os
+
 from pathlib import Path
 from pickle import TRUE
 import django_heroku
@@ -27,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+suntehz)b$%#3^@!-fy2@16+#e#r&=5(^$oz9)e#z-l+l^bpy'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = TRUE
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -133,14 +133,4 @@ STATICFILES_STORAGE= 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATIC_ROOT =os.path.join(BASE_DIR,'staticfiles')
-
-STATIC_TMP= os.path.join(BASE_DIR,'static')
-STATIC_URL = '/static/'
-
-os.makedirs(STATIC_TMP,exist_ok=TRUE)
-os.makedirs(STATIC_ROOT,exist_ok=True)
-
-STATICFILES_DIRS =(
-    os.path.join(BASE_DIR,'static'),
-)
+django_heroku.settings(locals())
